58854cbbf2e60cdc31d74e2ab3ff6a7c89065a43
JGrndn <jerome.grondin.fr@gmail.com>
2019-01-15 11:16:51 +0100

    Update cwLayoutCustomQuery.js

--- src/cwLayoutCustomQuery.js
+++ src/cwLayoutCustomQuery.js
@@ -272,7 +272,7 @@
       // layout options
       that.optionsManager.setLayoutOptions(that.options.CustomOptions);
 
-      loader.loadControllerWithTemplate('cwCustomQueryController', $container, templatePath, function ($scope) {
+      loader.loadControllerWithTemplate('cwCustomQueryController', $container, templatePath, function ($scope, $sce) {
         $scope.objectId = that.objectId;
         $scope.node = that;
         $scope.templates = {
@@ -336,6 +336,10 @@
           $scope.chart.labels = data.labels;
           $scope.chart.data = data.data;
           $scope.chart.series = data.series;
+        };
+
+        $scope.getDisplayItem = function(item){
+          return $sce.trustAsHtml(item.displayName);
         };
 
         $scope.applyFilters();
